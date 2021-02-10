d634f6d320624a4fe9ede811afc2a36c3f29d1ac
yaowenqiang <yaowenqiang111@163.com>
2021-02-10 14:36:52 +0800

    add absic crud demo

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
     "gorm.io/gorm"
     "gorm.io/driver/postgres"
+    "fmt"
 )
 
 func main() {
@@ -26,6 +27,79 @@ func main() {
         panic(err.Error())
     }
 
-    println("Connection to database established")
+    //println("Connection to database established")
 
+    db.AutoMigrate(&People{})
+
+    user := People{
+        Username: "jack",
+        FirstName: "jack",
+        LastName: "yao",
+    }
+
+    db.Create(&user)
+
+
+    var users = []People{
+        People{
+            Username: "Adent",
+            FirstName: "Arthur",
+            LastName: "Dent",
+        },
+        People{
+            Username: "fprefect",
+            FirstName: "Ford",
+            LastName: "Prefect",
+        },
+        People{
+            Username: "tmacmillan",
+            FirstName: "Tricia",
+            LastName: "MacMillan",
+        },
+        People{
+            Username: "mrobot",
+            FirstName: "Marvin",
+            LastName: "Robot",
+        },
+    }
+
+    for _, u := range users{
+        db.Create(&u)
+    }
+
+    uu  := People{}
+    //db.First(&uu)
+    db.Last(&uu)
+    fmt.Println(uu)
+    //println("done")
+
+    //fmt.Println(user)
+    updateUser := People{
+        Username: "jack",
+    }
+
+    db.Where(&updateUser).First(&updateUser)
+
+    fmt.Println(updateUser)
+
+    updateUser.LastName = "LastMan"
+
+    db.Save(&updateUser)
+
+
+    updatedUser := People{}
+    db.Where(&updateUser).First(&updatedUser)
+    fmt.Println(updatedUser)
+
+
+    db.Where(&People{Username:"jack"}).Delete(&People{})
+
+    fmt.Println("done")
+}
+
+type People struct {
+    ID uint
+    Username string
+    FirstName  string
+    LastName string
 }
