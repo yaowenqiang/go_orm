c6d7765b585206e2d8f901c5ede7053288d52994
yaowenqiang <yaowenqiang111@163.com>
2021-02-12 19:58:10 +0800

    add one2Many insert and query

--- src/relation.go
+++ src/relation.go
@@ -16,7 +16,7 @@ func main() {
 	  logger.Config{
 		SlowThreshold: time.Second,   // 慢 SQL 阈值
 		LogLevel:      logger.Silent, // Log level
-		Colorful:      false,         // 禁用彩色打印
+		Colorful:      true,         // 禁用彩色打印
 	  },
 	)
     dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
@@ -45,6 +45,7 @@ func main() {
 
     db.AutoMigrate(&User{})
     db.AutoMigrate(&Calendar{})
+    db.AutoMigrate(&Appointment{})
 
     user := User{
         Username: "jack",
@@ -52,22 +53,38 @@ func main() {
         LastName: "yao",
         Calendar: Calendar{
             Name: "Improbable Events",
+            Appointments: []Appointment{
+                {
+                    Subject:"meet with jack ma",
+                },
+                {
+                    Subject:"meet with tony ma",
+                },
+                {
+                    Subject:"meet with jack chen",
+                },
+            },
         },
     }
 
-    db.Create(&user)
+    db.Debug().Create(&user)
 
     u := User{}
     c := Calendar{}
+    //a := []Appointment{}
 
-    db.First(&u)
+    db.Debug().First(&u)
 
-    db.Debug().Model(&u).Association("Calendar").Find(&c)
+    //db.Debug().Model(&Calendar{}).AddForeignKey("user_id","users(id)", "cascade", "casedade")
 
-    fmt.Println(u)
+    //db.Debug().Model(&u).Association("Calendar").Find(&c)
+    db.Debug().Preload("Appointments").Find(&c)
+    //db.Debug().Model(&c).Association("Appointment").Find(&a)
+
+    fmt.Printf("user.calendar: %+v\n", u.Calendar.Name)
     fmt.Println(c)
-
-
+    //fmt.Println(a)
+    db.Debug().Delete(&u)
 
     fmt.Println("done")
 }
@@ -78,6 +95,8 @@ type User struct {
 	FirstName  string `gorm:"size:15;not null;column:FirstName"`
 	LastName string `gorm:"unique;uniqueIndex;not null;column:LastName;default:smith"`
     Calendar Calendar `gorm:"foreignKey:UserID"`
+    //Calendar Calendar
+    //CalendarID uint `gorm:"foreignKey:CalendarID"`
 }
 
 
@@ -85,6 +104,16 @@ type Calendar struct {
     gorm.Model
     Name string
     UserID uint
+    Appointments []Appointment
 }
 
 
+
+type Appointment struct {
+    gorm.Model
+    Subject string
+    Description string
+    StartTime time.Time
+    Length uint
+    CalendarID uint
+}
